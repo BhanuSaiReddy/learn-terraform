5de93b1cffafb79499b1b06d34a3db64168db4ac
bhanu <bhanusaireddy5244@gmail.com>
2023-10-21 12:43:38 +0530

    terraform

--- 06-lopps/count/main.tf
+++ 06-lopps/count/main.tf
@@ -9,6 +9,7 @@ resource "aws_instance" "instance" {
   instance_type = "t3.small"
   vpc_security_group_ids = [ "sg-00f89e3d7e55eac8b" ]
   tags = {
-    Name = ""
+    #Name = var.components [count.index]
+    Name = element (var.components , count.index)
   }
 }
